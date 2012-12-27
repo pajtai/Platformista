fb96c3235a514b12270552c3a25d3394ba56ad7f
Peter Ajtai <peter.ajtai@gmail.com>
2012-12-26 23:48:54 -0800

    Create Tilemap.js

---
+++ application/models/Tilemap.js
@@ -0,0 +1,23 @@
+(function(Backbone, plat){
+
+    plat.namespace('models').Tilemap = Backbone.Model.extend({
+
+        defaults: {
+
+            // size of map in number of tiles
+            width: 100,
+            height: 100,
+
+            // size of each cell in pixels
+            cellSize: 32,
+
+            // matrix of tiles
+            map: [],
+
+            // todo: validate map to w & h
+
+            // mapping of map symbols to images or sprite squares
+            mapDefinitions: {}
+        }
+    });
+}(Backbone, window.plat));
