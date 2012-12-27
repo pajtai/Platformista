9632585ef0a21321dfd02b576dd2b7c2115429dc
Peter Ajtai <peter.ajtai@gmail.com>
2012-12-26 23:53:06 -0800

    Create Being.js

---
+++ application/models/Being.js
@@ -0,0 +1,13 @@
+(function(Backbone, plat){
+
+    plat.namespace('models').Being = Backbone.Model.extend({
+
+        defaults: {
+
+            x: 0,
+            y: 0,
+
+            velocity: [0, 0, 0]
+        }
+    });
+}(Backbone, window.plat));
